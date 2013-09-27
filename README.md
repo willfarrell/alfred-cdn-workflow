c5bb73a0a8d3134b306276b40957be446e28547d
will Farrell <will.farrell@gmail.com>
2013-09-27 14:43:37 -0600

    beta src

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-alfred-cdn-workflow
-===================
+CDN ([Download v1.0](https://raw.github.com/willfarrell/alfred-cdn-workflow/master/CDN.alfredworkflow))
+=====================
 
 Check which CDNs a package is hosted on.
+
+## Requirements
+1. [Alfred App v2](http://www.alfredapp.com/#download)
+1. [Alfred Powerpack](https://buy.alfredapp.com/)
+
+## Installing
+1. Click the download buttons below
+2. Double-click to import into Alfred 2
+3. Review the workflow to add custom Hotkeys
+
+## Updating
+Run the [Alleyoop Workflow](http://www.alfredforum.com/topic/1582-alleyoop-update-alfred-workflows/) using the keyword `oop`. If you're not comfortable with Alleyoop, **star & watch this repo** to keep up to date on new versions and additional workflows.
+
+## About
+
+![alt text][sample]
+
+## Commands
+- `cdn {query}`
+
+## Contributors
+- [@willfarrell](https://github.com/willfarrell)
+
+[sample]: ./screenshots/sample.png "Sample"
---
+++ src/cdn.php
@@ -0,0 +1,127 @@
+<?php
+//$query = "jq";
+$query = "cf jquery";
+//$query = "msn jq";
+// ****************
+//error_reporting(0);
+require_once('workflows.php');
+
+$w = new Workflows();
+if (!isset($query)) { $query = "{query}"; }
+$query = strtolower(trim($query));
+
+$cdns = json_decode(file_get_contents("cdns.json"));
+$output = array();
+
+if (strpos($query, " ") !== false) {
+	$parts = explode(" ", $query);
+	$cdn_q = array_shift($parts);
+	$string = implode($parts);
+	
+	foreach($cdns as $cdn => $params) {
+		$count = count( $w->results() );
+		$pos = strpos($cdn, $cdn_q);
+		if ($pos !== false && $pos == 0) {
+			run($params, $string);
+			if ($count == count($w->results())) {
+				$w->result( "cdn-$cdn", $query, 'No libraries found.', $query, "icon-cache/$cdn.png", 'no' );
+			}
+		}
+		
+	}
+	
+}
+
+if ( count( $w->results() ) == 0 ) {
+	foreach($cdns as $cdn => $params) {
+		$count = count( $w->results() );
+		run($params, $query);
+		if ($count == count($w->results())) {
+			$w->result( "cdn-$cdn", $query, "{$params->name}", 'No libraries found.', "icon-cache/$cdn.png", 'no' );
+		}
+	}
+}
+
+function load($params) {
+	global $w;
+	// get db
+	$pkgs = $w->read("{$params->id}CDN.json");
+	$timestamp = $w->filetime("{$params->id}CDN.json");
+	if ((!$pkgs || $timestamp < (time() - 7 * 86400)) && $params->db_url) {
+		$data = $w->request($params->db_url);
+		$w->write($data, "{$params->id}CDN.json");
+		$pkgs = json_decode( $data );
+	} else if (!$pkgs) {
+		$data = '{"packages":[]}';
+		$pkgs = json_decode( $data );
+	}
+	
+	$pkgs = $pkgs->packages;
+	return $pkgs;
+}
+
+function search($plugin, $query) {
+	
+	if (isset($plugin->name)) {
+		$name = strtolower(trim($plugin->name));
+		if (strpos($name, $query) === 0) {
+			return 1;
+		} else if (strpos($name, $query) > 0) {
+			return 2;
+		} else if(strpos($name, $query) !== false) {
+			return 3;
+		} else if (isset($plugin->description) && strpos(strtolower(trim($plugin->description)), $query) !== false) {
+			return 4;
+		} else if (isset($plugin->keywords)) {
+			foreach($plugin->keywords as $keyword) {
+				if (strpos(strtolower(trim($keyword)), $query) !== false) {
+					return 5;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+function run($params, $query) {
+	global $w;
+	
+	$pkgs = load($params);
+	$output = array();
+	for($i = 0, $l = sizeof($pkgs); $i < $l; $i++) {
+		$pkg = $pkgs[$i];
+		$priority = search($pkg, $query);
+		if ($priority) {
+			$title = $pkg->name." (".$pkg->version.")"; // remove grunt- from title
+		
+			$url = "{$params->url}/{$pkg->name}/{$pkg->filename}";
+			
+			//$w->result( "cdn-{$params->id}-{$pkg->name}", $url, $title, $pkg->description, "icon-cache/{$params->id}.png", "yes" );
+			$output[$priority][] = array(
+				"id" => "cdn-{$params->id}-{$pkg->name}",
+				"value" => $url,
+				"title" => $title,
+				"details" => $pkg->description,
+				"icon" => "icon-cache/{$params->id}.png"
+			);
+			
+		}
+	}
+	
+	// print out order
+	$count = 15;
+	foreach($output as $list) {
+		foreach($list as $item) {
+			$w->result( $item["id"], $item["value"], $item["title"], $item["details"], $item["icon"], "yes" );
+			if (!--$count) { break; }
+		}
+	}
+}
+
+/*if ( count( $w->results() ) == 0 ) {
+	$w->result( 'cdn', $query, 'No libraries found.', $query, 'icon.png', 'no' );
+}*/
+
+echo $w->toxml();
+// ****************
+?>
